df803fa574b6adb9d4d9e5e84ebf48c095b3c97f
Daniel Nogueira <dnogueira@ipca.pt>
2026-02-27 17:56:49 +0000

    Add sprites

--- Sokoban/Game1.cs
+++ Sokoban/Game1.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using System.IO;
 
 namespace Sokoban
 {
@@ -10,6 +11,13 @@ namespace Sokoban
         private SpriteBatch _spriteBatch;
 
         private SpriteFont font;
+        
+        private int nrLinhas = 0;
+        private int nrColunas = 0;
+        private char[,] level;
+        
+        private Texture2D player, dot, box, wall; //Load images Texture 
+        int tileSize = 64; //potencias de 2 (operações binárias)
 
 
         public Game1()
@@ -17,11 +25,15 @@ namespace Sokoban
             _graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
             IsMouseVisible = true;
+            //_graphics.PreferredBackBufferWidth = nrColunas*tileSize; //define a largura da janela
+            //_graphics.PreferredBackBufferHeight = nrLinhas*tileSize; //define a altura da janela        
         }
 
         protected override void Initialize()
         {
             // TODO: Add your initialization logic here
+            
+            LoadLevel("level1.txt");
 
             base.Initialize();
         }
@@ -30,6 +42,11 @@ namespace Sokoban
         {
             _spriteBatch = new SpriteBatch(GraphicsDevice);
             font = Content.Load<SpriteFont>("FontSokoban"); //Use the name of sprite font file ('File')
+            player = Content.Load<Texture2D>("Character4");
+            dot = Content.Load<Texture2D>("EndPoint_Blue");
+            box = Content.Load<Texture2D>("Crate_Brown");
+            wall = Content.Load<Texture2D>("Wall_Brown");
+
 
             // TODO: use this.Content to load your game content here
         }
@@ -50,12 +67,62 @@ namespace Sokoban
 
             // TODO: Add your drawing code here
             _spriteBatch.Begin();
-
-            _spriteBatch.DrawString(font, "O texto que quiser", new Vector2(100, 100), Color.White);
             
+            //_spriteBatch.DrawString(font, "O texto que quiser", new Vector2(0, 40), Color.White);
+            //_spriteBatch.DrawString(font, $"Numero de Linhas = {nrLinhas} -- Numero de Colunas = {nrColunas}", new Vector2(0, 0), Color.White);
+
+            Rectangle position = new Rectangle(0, 0, tileSize, tileSize); //calculo do retangulo a depender do tileSize
+            for (int x = 0; x < level.GetLength(0); x++)  //pega a primeira dimensão
+            {
+                for (int y = 0; y < level.GetLength(1); y++) //pega a segunda dimensão
+                {
+                    position.X = x * tileSize; // define o position
+                    position.Y = y * tileSize; // define o position
+
+                    switch (level[x, y])
+                    {
+                        case 'Y':
+                            _spriteBatch.Draw(player, position, Color.White);
+                            break;
+                        case '#':
+                            _spriteBatch.Draw(box, position, Color.White);
+                            break;
+                        case '.':
+                            _spriteBatch.Draw(dot, position, Color.White);
+                            break;
+                        case 'X':
+                            _spriteBatch.Draw(wall, position, Color.White);
+                            break;
+                    }
+                }
+            }
+
+
             _spriteBatch.End();
+
 
             base.Draw(gameTime);
         }
+
+        void LoadLevel(string levelFile)
+        {
+            //Função para ler o ficheiro do nível e armazenar os dados numa matriz de caracteres
+   
+            string[] linhas = File.ReadAllLines($"Content/{levelFile}");  // "Content/" + level
+            nrLinhas = linhas.Length;
+            nrColunas = linhas[0].Length;
+
+            level = new char[nrColunas, nrLinhas];
+
+            for (int x = 0; x < nrColunas; x++)
+            {
+                for (int y = 0; y < nrLinhas; y++)
+                {
+                    level[x, y] = linhas[y][x];
+                }
+            }
+
+        }
+
     }
 }
